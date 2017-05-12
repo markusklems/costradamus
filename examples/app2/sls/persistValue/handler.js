1f68a2be95eaf30b5e06c779a981594b0a3606bf
j.kuhlenkamp <j.kuhlenkamp@tu-berlin.de>
2017-05-12 16:36:24 +0200

    Adding function PersistValue.

---
+++ examples/app2/sls/persistValue/handler.js
@@ -0,0 +1,28 @@
+/**
+ * Created by jk on 12.05.17.
+ */
+
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = (event, context, callback) => {
+
+    // TODO Check params
+
+    const params = {
+        TableName: 'ValuesTable',
+        Item: {
+            id: event.id,
+            timestamp: event.timestamp,
+            value: event.value
+        },
+        ReturnConsumedCapacity: "TOTAL"
+    };
+
+    return dynamo.put(params, (err, data) => {
+        if(err) callback(err);
+        else callback(null, "Success!");
+    });
+};
