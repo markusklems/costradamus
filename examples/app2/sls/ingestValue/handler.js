4b350ebd31baf1961c2980605bdf76dfe56dd8f4
j.kuhlenkamp <j.kuhlenkamp@tu-berlin.de>
2017-05-15 17:43:53 +0200

    Adding AWS SQS topics for different time series and extending the AWS Lambda function ingestValue with the messaging pattern Message-Router. Changing the invocation pattern for all external calls to async and adding Promise-based fork/join.

--- examples/app2/sls/ingestValue/handler.js
+++ examples/app2/sls/ingestValue/handler.js
@@ -6,27 +6,61 @@
 
 const AWS = require('aws-sdk');
 const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+
+const sendToDynamodb = event => {
+    return new Promise((resolve, reject) => {
+        const params = {
+            FunctionName: 'persistValueFunction',
+            InvocationType: 'RequestResponse',
+            Payload: JSON.stringify(event, null)
+        };
+        lambda.invoke(params, (err, data) => {
+            if (err) {
+                console.log(err, err.stack, err.message);
+                reject(err);
+            }
+            else {
+                console.log(data);
+                resolve(data);
+            }
+        });
+    });
+};
+
+const sendToSqs = event => {
+    return new Promise((resolve, reject) => {
+        const params = {
+            MessageBody: JSON.stringify(event),
+            QueueUrl: 'https://sqs.us-east-1.amazonaws.com/186706155491/DE00056366740S2031372170000000000010001080000'
+        };
+        sqs.sendMessage(params, (err, data) => {
+            if (err) {
+                console.log(err, err.stack);
+                reject(err);
+            }
+            else {
+                console.log(data);
+                resolve(data);
+            }
+        });
+    })
+};
 
 module.exports.handler = (event, context, callback) => {
 
     // TODO Check params
 
-    const persistParams = {
-        FunctionName: 'persistValueFunction',
-        InvocationType: 'RequestResponse',
-        Payload: JSON.stringify(event, null)
-    };
-    /*
-    const predictParams = {
-        FunctionName: "smartgridmonitor-dev-predictValueFunction",
-        InvocationType: "Event",
-        Payload: JSON.stringify(event)
-    };
-    */
-    lambda.invoke(persistParams, (err, data) => {
-        if (err) console.log(err, err.stack, err.message);
-        else     callback(null, data);
-    });
+    // TODO Join both functions and terminate via a callback.
+    const promises = [sendToDynamodb(event), sendToSqs(event)];
+    Promise.all(promises)
+        .then( values => {
+            callback(null, values);
+        })
+        .catch( err => {
+            callback(err);
+        });
 
     /**
     lambda.invoke(predictParams, function(err, data) {
