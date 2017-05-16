dbaa4201ff7e3776eacc73c6f226f9406ad8bf5a
j.kuhlenkamp <j.kuhlenkamp@tu-berlin.de>
2017-05-16 18:04:48 +0200

    Adding AWS Lambda function readNotifications with corresponding test event and documentation.

--- examples/app2/sls/readNotifications/handler.js
+++ examples/app2/sls/readNotifications/handler.js
@@ -6,68 +6,61 @@
 
 const AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis({apiVersion: '2013-12-02'});
+const util = require('util');
+
+const getShardIterator = (streamName, from, shardId) => {
+    if (shardId === undefined) shardId = 'shardId-000000000000';
+    return new Promise((resolve, reject) => {
+        const params = {
+            ShardId: shardId,                       /* required */
+            ShardIteratorType: 'AT_TIMESTAMP',      /* required */
+            StreamName: streamName,                 /* required */
+            Timestamp: from
+        };
+        kinesis.getShardIterator(params, (err, data) => {
+            if (err) reject(err);
+            else{
+                // console.log('Result from getShardIterator(): ' +util.inspect(data));
+                resolve(data.ShardIterator);
+            }
+        });
+    })
+};
+
+const getRecords = shardIterator => {
+    return new Promise((resolve, reject) => {
+        const params = {
+            ShardIterator: shardIterator            /* required */
+        };
+        kinesis.getRecords(params, (err, data) => {
+            if (err) reject(err);
+            else {
+                // console.log('Result from getRecords(): ' +util.inspect(data));
+                const notifis = [],
+                records = Array.from(data.Records);
+                // console.log('Records: ' +util.inspect(records));
+                for(let r in records) {
+                    notifis.push(JSON.parse(records[r].Data));
+                }
+                // console.log('Notfications: ' +util.inspect(notifis));
+                resolve(notifis);
+            }
+        });
+
+    })
+};
 
 module.exports.handler = (event, context, callback) => {
 
-    // TODO Check params of client request.
-
-    const timeout = Math.floor((Math.random() * 1000) +200);
-    setTimeout(() => {
-        console.log('Event delivered: ' +event);
-    }, timeout);
-
-    callback(null);
-
-    /**
-    const streamParams = {
-      StreamName: 'NotificationsStream'
-    };
-
-    let shards = [], iterators = [];
-
-
-    // TODO End callback hell and use promises.
-    kinesis.describeStream( streamParams, (err, data) => {
-        if(err) callback(err);
-        else {
-            console.log('StreamDescription: ' +data.StreamDescription);
-            console.log('Shards: ' +data.StreamDescription.Shards);
-            console.log('Shards[0]: ' +data.StreamDescription.Shards[0]);
-            console.log('Shards[0].ShardId: ' +data.StreamDescription.Shards[0].ShardId);
-            for(let shard in data.StreamDescription.Shards) {
-                shards.push(shard.ShardId);
-                console.log('Shard: '+shard);
-                console.log('ShardId: '+shard.ShardId);
-            }
-            for(let shardId in shards) {
-                const iteratorParams = {
-                    ShardId: data.StreamDescription.Shards[0].ShardId,
-                    ShardIteratorType: 'LATEST',
-                    StreamName: 'NotificationsStream'
-                    // Timestamp: (event.start)
-                };
-                console.log('Timestamp: ' +event.start+ ', now: ' +(new Date().getTime()));
-                kinesis.getShardIterator( iteratorParams, (err, data) => {
-                    if(err) callback(err);
-                    else {
-                        iterators.push(data.ShardIterator);
-                        console.log('ShardIterator: ' +data.ShardIterator);
-                        // TODO This will not work for streams with more than a single shard. Consider to change this
-                        // to an implementation that supports multiple shards.
-                        for(let iterator in iterators) {
-                            const getParams = {
-                                ShardIterator: iterator
-                            };
-
-                            kinesis.getRecords(getParams, (err, data) => {
-                                if(err) callback(err);
-                                else callback(null, data);
-                            });
-                        }
-                    }
-                });
-            }
-        }
+    getShardIterator('NotificationsStream', event.start)
+    .then( shardIterator => {
+        return getRecords(shardIterator);
+    })
+    .then( records => {
+        callback(null, records);
+    })
+    .catch(err => {
+        callback(err)
     });
-    */
+
 };
