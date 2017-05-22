3ce9916c435ffb830e2a40779c37c166f954dc32
Markus Klems <markus@Markuss-MacBook-Pro-4.local>
2017-05-22 13:18:30 +0200

    Updated persistValue function: toggle tracing on/off.

--- examples/app2/sls/persistValue/handler.js
+++ examples/app2/sls/persistValue/handler.js
@@ -3,9 +3,16 @@
  */
 
 'use strict';
-const AWSXRAY = require('aws-xray-sdk-core');
-AWSXRAY.middleware.setSamplingRules('./sampling-rules.json');
-const AWS = AWSXRAY.captureAWS(require('aws-sdk'));
+let _tracing = true;
+let AWS, AWSXRAY;
+if (_tracing) {
+  AWSXRAY = require('aws-xray-sdk-core');
+  AWSXRAY.middleware.setSamplingRules('./sampling-rules.json');
+  AWS = AWSXRAY.captureAWS(require('aws-sdk'));
+} else {
+  AWS = require('aws-sdk');
+}
+
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => {
@@ -18,9 +25,12 @@ module.exports.handler = (event, context, callback) => {
       id: event.id,
       timestamp: event.timestamp,
       value: event.value
-    },
-    ReturnConsumedCapacity: "TOTAL"
+    }
   };
+
+  if (_tracing) {
+    params.ReturnConsumedCapacity = 'TOTAL';
+  }
 
   return dynamo.put(params, (err, data) => {
     if (err) callback(err);
