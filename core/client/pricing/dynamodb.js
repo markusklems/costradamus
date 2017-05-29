5731cd6a917d5343cf1ed92be69f451d7542a5fe
Jörn Kuhlenkamp <j.kuhlenkamp@tu-berlin.de>
2017-05-29 17:59:10 +0200

    Adding AWS DynamoDB support to costmodeller.

---
+++ core/client/pricing/dynamodb.js
@@ -0,0 +1,87 @@
+/**
+ * Created by jk on 24.05.17.
+ */
+
+"use strict";
+
+// Price for 1 mio write capacity units per hour in USD, SOURCE: (https://aws.amazon.com/dynamodb/pricing/, accessed: 2017/05/29)
+const _prices = {
+    'us-west-1': {
+        WCU: 650,
+        RCU: 130
+    },
+    'eu-west-1': {
+        WCU: 735,
+        RCU: 147
+    },
+    'eu-central-1': {
+        WCU: 735,
+        RCU: 147
+    }
+};
+
+const _price = (region, type) => {
+  return _prices[region][type];  
+};
+
+/**
+ * Models cost and waste metrics for a single invocation based on a set of consumption metrics.
+ *
+ * @param c                     Consumption object.
+ * @param c.PayloadSize         Size of the item that is written to or queried from a DynamoDB table.
+ *                              Example: { val: 1.5, type: 'KB' }
+ * @param c.CapacityUnits       Number of capacity units that are consumed by the operation.
+ *                              Example: { val: 2, type: 'WCU' }
+ * @param c.Latency             Request-Response latency of the operation.
+ *                              Example: { val: 800, type: 'MS' }
+ */
+module.exports = c => {
+
+    let costs = {};
+
+    const region = 'us-west-1';
+
+    if ( c.PayloadSize.type !== 'KB') {
+        new Error('InvalidParameterError: Payload must be specified in KB.' + c);
+    }
+
+    if ( c.CapacityUnits.type !== 'WCU' || c.CapacityUnits.type !== 'RCU') {
+        new Error('InvalidParameterError: Durations must be one of [WCU, RCU].' + c);
+    }
+
+    if ( c.Latency.type !== 'MS') {
+        new Error('InvalidParameterError: Durations must be specified in MS.' + c);
+    }
+
+    if (isNaN(c.PayloadSize.val) || isNaN(c.CapacityUnits.val) || isNaN(c.Latency.val)) {
+        new Error('InvalidParameterError: ' + c);
+    }
+
+    if (typeof c.PayloadSize.val === 'string')      c.PayloadSize.val = +c.PayloadSize.val;
+    if (typeof c.CapacityUnits.val === 'string')    c.CapacityUnits.val = +c.CapacityUnits.val;
+    if (typeof c.Latency.val === 'string')          c.Latency.val = +c.Latency.val;
+
+    // monetary operation cost
+    costs.MonetaryCost = {};
+    costs.MonetaryCost.type = 'USD';
+    costs.MonetaryCost.val = (c.CapacityUnits.val * c.Latency.val * _price('eu-west-1', c.CapacityUnits.type)) / 3600000;
+    console.log('MonetaryCost [USD]: ' +costs.MonetaryCost.val);
+
+    // Runtime waste
+    costs.PayloadWaste = {};
+    costs.PayloadWaste.type = 'KB';
+    if ( c.CapacityUnits.type === 'WCU' )
+        costs.PayloadWaste.val = c.CapacityUnits.val - c.PayloadSize.val;
+    if ( c.CapacityUnits.type === 'RCU')
+        costs.PayloadWaste.val = c.CapacityUnits.val * 4 - c.PayloadSize.val;
+    console.log('PayloadWaste [KB]: ' +costs.PayloadWaste.val);
+
+    // monetary runtime waste
+    // costs.MonetaryPayloadWaste = {};
+    // costs.MonetaryPayloadWaste.type = 'MS';
+    // costs.MonetaryPayloadWaste.val = costs.RuntimeWaste.val * _price(region, c.MemorySize.val);
+    // console.log('MonetaryRuntimeWaste: ' +costs.MonetaryRuntimeWaste.val);
+
+    return costs;
+
+};
---
+++ core/test/cm-dynamodb-test.js
@@ -0,0 +1,83 @@
+/**
+ * Created by Joern Kuhlenkamp on 29.05.17.
+ */
+
+'use strict';
+
+const chai = require('chai');
+const expect = require('chai').expect;
+
+const model = require('../client/pricing/dynamodb');
+
+describe('AWS DynamoDB Table (QUERY) - ', () => {
+
+    let input, output;
+
+    before( () => {
+        input = require('./in/dynamodb-query.json');
+        output = model( input );
+    });
+
+    it('MonetaryCost: 6KB, 2RCU => 0.098USD / 1 mio. req', done => {
+        expect(output.MonetaryCost.val).to.be.a('number');
+        expect(output.MonetaryCost.val).to.equal(0.098);
+        expect(output.MonetaryCost.type).to.be.a('string');
+        expect(output.MonetaryCost.type).to.equal('USD');
+        done();
+    });
+
+    it('PayloadWaste: 6KB, 2RCU => 2KB / req.', done => {
+        expect(output.PayloadWaste.val).to.be.a('number');
+        expect(output.PayloadWaste.val).to.equal(2);
+        expect(output.PayloadWaste.type).to.be.a('string');
+        expect(output.PayloadWaste.type).to.equal('KB');
+        done();
+    });
+
+    /**
+    it('MonetaryPayloadWaste: 6KB, 2RCU => { "val": , "type": "USD" }', done => {
+        expect(output.MonetaryRuntimeWaste.val).to.be.a('number');
+        expect(output.MonetaryRuntimeWaste.val).to.equal(0.000065);
+        expect(output.MonetaryRuntimeWaste.type).to.be.a('string');
+        expect(output.MonetaryRuntimeWaste.type).to.equal('USD');
+        done();
+    });
+     */
+
+});
+
+describe('AWS DynamoDB Table (PUT) - ', () => {
+
+    let input, output;
+
+    before( () => {
+        input = require('./in/dynamodb-put.json');
+        output = model( input );
+    });
+
+    it('MonetaryCost: 2.5KB, 3RCU => 0.49USD / 1 mio req.', done => {
+        expect(output.MonetaryCost.val).to.be.a('number');
+        expect(output.MonetaryCost.val).to.equal(0.49);
+        expect(output.MonetaryCost.type).to.be.a('string');
+        expect(output.MonetaryCost.type).to.equal('USD');
+        done();
+    });
+
+    it('PayloadWaste: 2.5KB, 3RCU => 0.5 KB / req.', done => {
+        expect(output.PayloadWaste.val).to.be.a('number');
+        expect(output.PayloadWaste.val).to.equal(0.5);
+        expect(output.PayloadWaste.type).to.be.a('string');
+        expect(output.PayloadWaste.type).to.equal('KB');
+        done();
+    });
+
+    /**
+    it('MonetaryPayloadWaste: 1.5KB, 2RCU => { "val": , "type": "USD" }', done => {
+        expect(output.MonetaryRuntimeWaste.val).to.be.a('number');
+        expect(output.MonetaryRuntimeWaste.val).to.equal(0.000325);
+        expect(output.MonetaryRuntimeWaste.type).to.be.a('string');
+        expect(output.MonetaryRuntimeWaste.type).to.equal('USD');
+        done();
+    });
+    */
+});
