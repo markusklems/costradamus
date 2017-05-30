2d62f787c90d71a4812c0e621336e6680c7c6627
Markus Klems <markus@Markuss-MacBook-Pro-4.local>
2017-05-30 14:06:35 +0200

    Removed a few Promises.

--- core/client/collector/collect.js
+++ core/client/collector/collect.js
@@ -36,57 +36,56 @@ let collect = document => {
 
 let makeCostDocument = (document) => {
   let costDocument = {};
-  let promises = [];
-  return new Promise((resolve, reject) => {
-    let lambdaDoc = finder.lambdaUsageFinder(document);
-    if (lambdaDoc) {
-      console.log("Found Lambda document");
-      promises.push(new Promise((pres, prej) => {
-        collectLambdaUsage(document).then(res => {
-          // TODO work in progress
-          //console.log("lambda res", res);
-          costDocument.id = document.id;
-          costDocument.parent_id = document.parent_id;
-          costDocument.resourceName = res.resourceName;
-          costDocument.resourceId = res.resourceId;
-          costDocument.consumptions = res.consumptions;
-          costDocument.cost = res.cost;
-          pres(true);
-        }).catch(err => reject(err));
-      }));
-    }
-    let dynamoDoc = finder.dynamoUsageFinder(document);
-    if (dynamoDoc) {
-      console.log("Found dynamodb document");
-      promises.push(new Promise((pres, prej) => {
-        let dynamoUsage = dynamoDoc.subsegments.find(finder.dynamoMetadataFinder);
-        dynamoUsage.metadata.DynamoDBConsumedCapacity.consumptions.Latency = dynamoDoc.end_time - dynamoDoc.start_time;
-        collectDynamodbUsage(dynamoUsage).then(res => {
-          // TODO work in progress
-          //console.log("res", res);
-          let metadata = res.metadata.DynamoDBConsumedCapacity;
-          let subsegment = {};
-          subsegment.id = dynamoDoc.id;
-          subsegment.name = dynamoDoc.name;
-          subsegment.resourceName = metadata.resourceName;
-          subsegment.consumptions = metadata.consumptions;
-          subsegment.cost = res.cost;
-          costDocument.subsegment = subsegment;
-          pres(true);
-        }).catch(err => reject(err));
-      }));
-    }
-    //console.log("document", document);
-    let kinesisDoc = finder.kinesisUsageFinder(document);
-    if (kinesisDoc) {
-      console.log("Found kinesis");
-    }
 
-    // Last but not least, resolve the Promise
-    Promise.all(promises).then(res => {
-      resolve(costDocument);
-    }).catch(err => console.error(err));
-  });
+  let lambdaDoc = finder.lambdaUsageFinder(document);
+  if (lambdaDoc) {
+    console.log("Found Lambda document");
+
+    collectLambdaUsage(document).then(res => {
+      // TODO work in progress
+      //console.log("lambda res", res);
+      costDocument.id = document.id;
+      costDocument.parent_id = document.parent_id;
+      costDocument.resourceName = res.resourceName;
+      costDocument.resourceId = res.resourceId;
+      costDocument.consumptions = res.consumptions;
+      costDocument.cost = res.cost;
+      pres(true);
+    }).catch(err => reject(err));
+
+  }
+  let dynamoDoc = finder.dynamoUsageFinder(document);
+  if (dynamoDoc) {
+    console.log("Found dynamodb document");
+
+    let dynamoUsage = dynamoDoc.subsegments.find(finder.dynamoMetadataFinder);
+    dynamoUsage.metadata.DynamoDBConsumedCapacity.consumptions.Latency = dynamoDoc.end_time - dynamoDoc.start_time;
+    collectDynamodbUsage(dynamoUsage).then(res => {
+      // TODO work in progress
+      //console.log("res", res);
+      let metadata = res.metadata.DynamoDBConsumedCapacity;
+      let subsegment = {};
+      subsegment.id = dynamoDoc.id;
+      subsegment.name = dynamoDoc.name;
+      subsegment.resourceName = metadata.resourceName;
+      subsegment.consumptions = metadata.consumptions;
+      subsegment.cost = res.cost;
+      costDocument.subsegment = subsegment;
+
+    }).catch(err => reject(err));
+
+  }
+  //console.log("document", document);
+  let kinesisDoc = finder.kinesisUsageFinder(document);
+  if (kinesisDoc) {
+    console.log("Found kinesis");
+  }
+
+  // Last but not least, resolve the Promise
+  Promise.all(promises).then(res => {
+    resolve(costDocument);
+  }).catch(err => console.error(err));
+
 }
 
 module.exports = collect;
