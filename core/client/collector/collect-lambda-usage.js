54cdebfb9648c4741b75690d450d6e22cd770165
Markus Klems <markus@Markuss-MacBook-Pro-4.local>
2017-05-30 18:14:07 +0200

    Refactored lambda usage collector.

--- core/client/collector/collect-lambda-usage.js
+++ core/client/collector/collect-lambda-usage.js
@@ -5,20 +5,17 @@ const parseCloudWatchLogs = require('./cw.js');
 const cost = require('../pricing/lambda.js');
 
 async function collectLambdaUsage(document) {
-  let lambdaUsage = document;
-  //console.log("lambdaUsage", lambdaUsage);
-  const errMsg = `A problem occured while processing the X-Ray data of ${lambdaUsage.name}. Expected that the 'AWS::Lambda::Function' segment has a subsegment with name 'LambdaMetadata' but could not find it. Skip processing this Lambda function...`;
-  if (lambdaUsage.subsegments) {
-    let lambdaMetadata = lambdaUsage.subsegments.find(finder.lambdaMetadataFinder);
+  const errMsg = `A problem occured while processing the X-Ray data of ${document.name}. Expected that the 'AWS::Lambda::Function' segment has a subsegment with name 'LambdaMetadata' but could not find it. Skip processing this Lambda function...`;
+  if (document.subsegments) {
+    let lambdaMetadata = document.subsegments.find(finder.lambdaMetadataFinder);
     if (lambdaMetadata) {
-      //console.log("lambdaMetadata", lambdaMetadata);
       const requestId = lambdaMetadata.metadata.ResourceUsage.RequestId;
       // add 5 seconds buffer before start and after end time
       // (to make sure that we catch the time window in cloudwatch that contains our request)
-      let startTimeUnix = parseInt(lambdaUsage.start_time.toString().replace(/\./, '')) - 5000;
-      let endTimeUnix = parseInt(lambdaUsage.end_time.toString().replace(/\./, '')) + 5000;
-      const resourceId = lambdaUsage.aws.function_arn;
-      const resourceName = lambdaUsage.name;
+      let startTimeUnix = parseInt(document.start_time.toString().replace(/\./, '')) - 5000;
+      let endTimeUnix = parseInt(document.end_time.toString().replace(/\./, '')) + 5000;
+      const resourceId = document.aws.function_arn;
+      const resourceName = document.name;
       //console.log("resourceName", resourceName)
       //console.log('startTimeUnix', new Date(startTimeUnix));
       //console.log('endTimeUnix', new Date(endTimeUnix));
@@ -26,18 +23,9 @@ async function collectLambdaUsage(document) {
 
       let res = {};
       try {
-        const util = require('util');
         let res = await parseCloudWatchLogs(resourceName, startTimeUnix, endTimeUnix, requestId);
-        console.log("INSPECTING the PROMISE");
-        console.log(util.inspect(res, false, null))
         // TODO
-        let costResult = cost(res); //= res ? cost(res) : 0;
-        console.log("DEBUGGG", {
-          "resourceName": resourceName,
-          "resourceId": resourceId,
-          "consumptions": res,
-          "cost": costResult
-        });
+        let costResult = cost(res);
         return new Promise((resolve, reject) => {
           resolve({
             "resourceName": resourceName,
