8c2b04839175367c65a804b865c5640400e34f6c
Jörn Kuhlenkamp <j.kuhlenkamp@tu-berlin.de>
2017-05-30 12:23:29 +0200

    Adding CM Kinesis support.

--- core/test/cm-kinesis-test.js
+++ core/test/cm-kinesis-test.js
@@ -18,21 +18,23 @@ describe('AWS Kinesis Stream (READ, Base Retention) - ', () => {
         output = model( input );
     });
 
-    it('MonetaryCost: 128KB, READ => ?', done => {
+    it('MonetaryCost: 256KB, READ => ~0.284', done => {
         expect(output.MonetaryCost.val).to.be.a('number');
-        // expect(output.MonetaryCost.val).to.equal(0.098);
+        expect(output.MonetaryCost.val.toFixed(3)).to.equal('0.284');
         expect(output.MonetaryCost.type).to.be.a('string');
         expect(output.MonetaryCost.type).to.equal('USD');
         done();
     });
 
-    it('PayloadWaste: 128KB, READ => ?', done => {
+    /**
+    it('PayloadWaste: 256KB, READ => ?', done => {
         expect(output.PayloadWaste.val).to.be.a('number');
         // expect(output.PayloadWaste.val).to.equal(2);
         expect(output.PayloadWaste.type).to.be.a('string');
         expect(output.PayloadWaste.type).to.equal('KB');
         done();
     });
+     */
 
 });
 
@@ -45,14 +47,15 @@ describe('AWS Kinesis Stream (WRITE, Base Retention) - ', () => {
         output = model( input );
     });
 
-    it('MonetaryCost: 128KB, WRITE => ?', done => {
+    it('MonetaryCost: 128KB, WRITE => ~0.226', done => {
         expect(output.MonetaryCost.val).to.be.a('number');
-        // expect(output.MonetaryCost.val).to.equal(0.49);
+        expect(output.MonetaryCost.val.toFixed(3)).to.equal('0.226');
         expect(output.MonetaryCost.type).to.be.a('string');
         expect(output.MonetaryCost.type).to.equal('USD');
         done();
     });
 
+    /**
     it('PayloadWaste: 128KB, WRITE => ?', done => {
         expect(output.PayloadWaste.val).to.be.a('number');
         // expect(output.PayloadWaste.val).to.equal(0.5);
@@ -60,5 +63,6 @@ describe('AWS Kinesis Stream (WRITE, Base Retention) - ', () => {
         expect(output.PayloadWaste.type).to.equal('KB');
         done();
     });
+     */
 
 });
