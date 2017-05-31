856bc62ce62ff470c780f03e3243b1972f7d200c
Markus Klems <markus@Markuss-MacBook-Pro-4.local>
2017-05-31 09:29:19 +0200

    Added Kinesis tracer.

--- core/tracer/kinesis-tracer.js
+++ core/tracer/kinesis-tracer.js
@@ -7,12 +7,23 @@ module.exports = class KinesisTracer {
   addSubsegment(parent, operation, params) {
     console.log("Add Kinesis Subsegment with operation type and payload size.");
     let subsegment = parent.addNewSubsegment("KinesisMetadata");
-    subsegment.addMetadata("Operation", operation, "ResourceUsage");
+    let consumptions = {};
     // Data is base64-encoded when the Data blob is serialized
     // Calculate payload size in bytes.
-    let payloadSize = Math.ceil(AWS.util.base64.encode(params.Data).length * 8 / 6);
-    //console.log("payloadSize", payloadSize);
-    subsegment.addMetadata("MessagePayloadSize", payloadSize, "ResourceUsage");
+    let payloadSizeInByte = Math.ceil(AWS.util.base64.encode(params.Data).length * 8 / 6);
+    let payloadSize = {
+      "val": payloadSizeInByte,
+      "type": "B"
+    }
+    let kinesisOp = {
+      "Operation": {
+        "val": operation,
+        "type": "KINESISOPERATION"
+      }
+    }
+    consumptions.PayloadSize = payloadSize;
+    consumptions.Operation = kinesisOp;
+    subsegment.addMetadata("consumptions", consumptions, "KinesisMetadata");
     subsegment.close();
   }
 }
